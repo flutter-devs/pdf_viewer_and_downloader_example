51668c6c0811857675727c8cd4743adf350c03b4
Shafquat Majid <shafquat@aeologic.com>
2022-05-20 20:49:45 +0530

    Pdf viewer and downloader

---
+++ lib/pdf_viewer_page.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:dio/dio.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class PdfViewerPage extends StatefulWidget {
+  final File file;
+  final String url;
+
+  const PdfViewerPage({
+    Key? key,
+    required this.file,
+    required this.url,
+  }) : super(key: key);
+
+  @override
+  State<PdfViewerPage> createState() => _PdfViewerPageState();
+}
+
+class _PdfViewerPageState extends State<PdfViewerPage> {
+  @override
+  Widget build(BuildContext context) {
+    final name = basename(widget.file.path);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await saveVideo(widget.url, "sample.pdf");
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text(
+                    'success',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              );
+            },
+            icon: const Icon(Icons.download_rounded),
+          ),
+        ],
+      ),
+      body: PDFView(
+        filePath: widget.file.path,
+      ),
+    );
+  }
+
+  Future<bool> saveVideo(String url, String fileName) async {
+    try {
+      if (await _requestPermission(Permission.storage)) {
+        Directory? directory;
+        directory = await getExternalStorageDirectory();
+        String newPath = "";
+        List<String> paths = directory!.path.split("/");
+        for (int x = 1; x < paths.length; x++) {
+          String folder = paths[x];
+          if (folder != "Android") {
+            newPath += "/" + folder;
+          } else {
+            break;
+          }
+        }
+        newPath = newPath + "/PDF_Download";
+        directory = Directory(newPath);
+
+        File saveFile = File(directory.path + "/$fileName");
+        if (kDebugMode) {
+          print(saveFile.path);
+        }
+        if (!await directory.exists()) {
+          await directory.create(recursive: true);
+        }
+        if (await directory.exists()) {
+          await Dio().download(
+            url,
+            saveFile.path,
+          );
+        }
+      }
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> _requestPermission(Permission permission) async {
+    if (await permission.isGranted) {
+      return true;
+    } else {
+      var result = await permission.request();
+      if (result == PermissionStatus.granted) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
